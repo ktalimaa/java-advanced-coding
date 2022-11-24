e5deeac9a7881e99eaa2a1b28ace03b7af17bb5a
ktalimaa <kristeltalimaa@gmail.com>
2022-11-24 20:46:03 +0200

    Person HW added

--- src/main/java/org/sda/java19/Main.java
+++ src/main/java/org/sda/java19/Main.java
@@ -1,7 +1,27 @@
 package org.sda.java19;
 
+/**
+ * Personal information:
+ * a. Create a file containing any personal data (name, surname, phone number). Data of individual persons should be in the following lines.
+ * b. Download data from a file and create objects of people based on them (in any way - Regex, String.split ...).
+ * c. Enter the created objects into ArrayList or Map (<line number>: <Person>).
+ * d. Present the obtained data.
+ *
+ * @author Kristel Talimaa
+ */
+
+// create txt file with info above
+// import the file
+// print the file, like here, like vehicle ex
+// load it into here
+// arraylist of person to make
+// finally print the person value
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+
     }
 }
