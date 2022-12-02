668c4b5b57d831f11527375035e76a010ded80a3
ktalimaa <kristeltalimaa@gmail.com>
2022-12-02 22:46:42 +0200

    Scheduler changed

--- src/main/java/org/sda/java19/Main.java
+++ src/main/java/org/sda/java19/Main.java
@@ -1,36 +1,40 @@
 package org.sda.java19;
 
+import org.sda.java19.exceptions.MaximumNumberOfStudentsReached;
 import org.sda.java19.models.Group;
 import org.sda.java19.models.Student;
 import org.sda.java19.models.Trainer;
 
+import javax.xml.crypto.Data;
 import java.time.LocalDate;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * SDA Scheduler
- *
+ * <p>
  * Create a class hierarchy:
  * Person.java - firstname  (inheritance)
- *             - lastname
- *             - dateOfBirth
+ * - lastname
+ * - dateOfBirth
  * Trainer.java (extends Person) - isAuthorized (boolean)
  * Student.java (extends Person) - hasPreviousJavaKnowledge (boolean)
- *
- *
+ * <p>
+ * <p>
  * Create a Group class which has
- *
+ * <p>
  * name (Java19Remote, Java20Remote, etc)
  * one trainer
  * a list of students
- *
- *
+ * <p>
+ * <p>
  * Manually initialize 15 students; 4 groups and 3 trainers;
  * Store all students in a list; all groups in a list; all trainers in a list;
  * Assign a trainer to each group
  * Assign 2-3 students to each group
- *
+ * <p>
  * Ensure the fact that a group will only have distinct students (How would you do that?) (each group has only unique students)
  * Ensure the fact that a group will only have a maximum of 5 students; When you try to add a 6th one throw an MaximumNumberOfStudentsReached exception
  * Display all students in a group sorted alphabetically by lastName // for all display ex use streams
@@ -58,8 +62,69 @@ public class Main {
 
         assignStudentsToGroup(groupList);
 
+        List<Student> studentsInGroup = getInitialStudents();
+        Collections.shuffle(studentList);
+        studentsInGroup.stream()
+                .collect(Collectors.groupingBy(Student:: toString))
+                .values()
+                .stream()
+                .map(x -> x.stream().map(Objects::toString)).collect(Collectors.toList())
+                .forEach(System.out::println);
+
+        if(studentsInGroup.size() <= 5) {
+            assignStudentsToGroup(groupList);
+        }
+        System.out.println(groupList);
+
+
+        /*
+
+        } catch (MaximumNumberOfStudentsReached maximumNumberOfStudentsReached) {
+        System.out.println(maximumNumberOfStudentsReached.getLocalizedMessage());
+    }
+
+    _________
+
+
+          groupList.add(new Group(Arrays.asList(assignStudentsToGroup(1)));
+
+         --------------
+
+        Collections.shuffle(studentList);
+        Map<String, List<Student>> grouping = (Map<String, List<Student>>) IntStream.range(0, studentList.size())
+                .boxed()
+                .collect(Collectors.groupingBy(i -> i % numberOfGroups))
+                .values()
+                .stream()
+                .map(x -> x.studentList::get).collect(Collectors.toList()));
+
+        System.out.println(grouping);
+
+
+           Map<String, List<Student>> grouping = studentList.stream()
+                .collect(Collectors.groupingBy(w -> w.getFirstName()));
+
+        System.out.println(grouping);
+
+        ----------
+
+        final int numberOfGroups = 4;
+
+        Collections.shuffle(studentList);
+        List<List<String>> groups = IntStream.range(0, studentList.size())
+                .boxed()
+                .collect(Collectors.groupingBy(i -> i % numberOfGroups))
+                .values()
+                .stream()
+                .map(x -> x.stream().map(studentList::get)).collect(Collectors.toList()))
+                .collect(Collectors.toList());
+
+        groups.forEach(System.out::println);
+
+         */
 
     }
+
     public static List<Student> getInitialStudents() {
 
         Student student = new Student();
@@ -95,67 +160,67 @@ public class Main {
         Student student5 = new Student();
         student5.setFirstName("Aleks");
         student5.setLastName("Siht");
-        student5.setDateOfBirth(LocalDate.of(1995, 5,26));
+        student5.setDateOfBirth(LocalDate.of(1995, 5, 26));
         student5.setHasPreviousJavaKnowledge(false);
 
         Student student6 = new Student();
         student6.setFirstName("Tony");
         student6.setLastName("Stark");
-        student6.setDateOfBirth(LocalDate.of(1988, 7,26));
+        student6.setDateOfBirth(LocalDate.of(1988, 7, 26));
         student6.setHasPreviousJavaKnowledge(true);
 
         Student student7 = new Student();
         student7.setFirstName("Super");
         student7.setLastName("Man");
-        student7.setDateOfBirth(LocalDate.of(1996, 2,13));
+        student7.setDateOfBirth(LocalDate.of(1996, 2, 13));
         student7.setHasPreviousJavaKnowledge(true);
 
         Student student8 = new Student();
         student8.setFirstName("Santa");
         student8.setLastName("Claus");
-        student8.setDateOfBirth(LocalDate.of(1980, 12,24));
+        student8.setDateOfBirth(LocalDate.of(1980, 12, 24));
         student8.setHasPreviousJavaKnowledge(true);
 
         Student student9 = new Student();
         student9.setFirstName("Captain");
         student9.setLastName("America");
-        student9.setDateOfBirth(LocalDate.of(2006, 10,8));
+        student9.setDateOfBirth(LocalDate.of(2006, 10, 8));
         student9.setHasPreviousJavaKnowledge(true);
 
         Student student10 = new Student();
         student10.setFirstName("Malle");
         student10.setLastName("Maasikas");
-        student10.setDateOfBirth(LocalDate.of(1990, 8,20));
+        student10.setDateOfBirth(LocalDate.of(1990, 8, 20));
         student10.setHasPreviousJavaKnowledge(true);
 
         Student student11 = new Student();
         student11.setFirstName("Kalle");
         student11.setLastName("Kaalikas");
-        student11.setDateOfBirth(LocalDate.of(1991, 9,11));
+        student11.setDateOfBirth(LocalDate.of(1991, 9, 11));
         student11.setHasPreviousJavaKnowledge(true);
 
         Student student12 = new Student();
         student12.setFirstName("Jüri");
         student12.setLastName("Juurikas");
-        student12.setDateOfBirth(LocalDate.of(2002, 1,25));
+        student12.setDateOfBirth(LocalDate.of(2002, 1, 25));
         student12.setHasPreviousJavaKnowledge(true);
 
         Student student13 = new Student();
         student13.setFirstName("Reet");
         student13.setLastName("Redis");
-        student13.setDateOfBirth(LocalDate.of(1999, 4,12));
+        student13.setDateOfBirth(LocalDate.of(1999, 4, 12));
         student13.setHasPreviousJavaKnowledge(false);
 
         Student student14 = new Student();
         student14.setFirstName("Peeter");
         student14.setLastName("Termomeeter");
-        student14.setDateOfBirth(LocalDate.of(1997, 3,9));
+        student14.setDateOfBirth(LocalDate.of(1997, 3, 9));
         student14.setHasPreviousJavaKnowledge(true);
 
         Student student15 = new Student();
         student15.setFirstName("Kala");
         student15.setLastName("Mees");
-        student15.setDateOfBirth(LocalDate.of(1996, 4,16));
+        student15.setDateOfBirth(LocalDate.of(1996, 4, 16));
         student15.setHasPreviousJavaKnowledge(true);
 
         return List.of(student, student1, student2, student3, student4, student5, student6, student7, student8, student9, student10, student11, student12, student13, student14, student15);
@@ -207,11 +272,7 @@ public class Main {
 
     private static void assignStudentsToGroup(List<Group> groupList) {
 
-        // group4.setStudentsList(getInitialStudents()); ??
-
 
 
     }
-
-
 }
